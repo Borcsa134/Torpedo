5531377d24357eb3cfd2baf7d99ea0e2a668c318
Borcsa134 <snakekid13@gmail.com>
2017-04-04 22:35:48 +0200

    Torpedo
    Android nagyházi feladat

---
+++ app/src/main/java/hu/bme/amorg/torpedo/MainActivity.java
@@ -0,0 +1,24 @@
+package hu.bme.amorg.torpedo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button newgame_button = (Button) findViewById(R.id.btn_newgame);
+        newgame_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+    }
+}
